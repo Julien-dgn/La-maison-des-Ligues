09630283e876343a5d94b9caf69eac0348d49ccd
Julien-dgn <julien.digeonsrvt@gmail.com>
2015-01-14 15:06:47 +0100

    delete

--- LIGUES/PlatformBundle/Resources/views/Default/footer.html.php
+++
@@ -1,5 +0,0 @@
-<br/><footer id= "footer">										<!-- Footer -->
-			<section id= "editeur_signature">
-					<br/>MAISON DES LIGUES -- Forum sous licence 
-			</section>
-	</footer>
--- LIGUES/PlatformBundle/Resources/views/Default/head.html.php
+++
@@ -1,6 +0,0 @@
-	
-
-	<head>
-		<title>Maison Des Ligue-Forum</title>
-		<?php include('color.html.php'); ?>
-	</head>
