94b0e463c5d9c29265dd992921fc2835ffea7527
Julien-dgn <julien.digeonsrvt@gmail.com>
2015-01-14 16:51:48 +0100

    erreur

---
+++ LIGUES/PlatformBundle/Resources/views/Default/footer.html.php
@@ -0,0 +1,5 @@
+<br/><br/><br/><br/><footer id= "footer">										<!-- Footer -->
+			<section id= "editeur_signature">
+					<br/>MAISON DES LIGUES -- Forum sous licence 
+			</section>
+	</footer>
